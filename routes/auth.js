fc990efd632ea3f0f4919a905a4c1782376726a8
Jovita Morkune <jovita.morkune@gmail.com>
2020-03-03 15:57:02 +0200

    Fix registration flash message bug

--- routes/auth.js
+++ routes/auth.js
@@ -12,18 +12,18 @@ router.get('/', (req, res) => {
 
 // show register form
 router.get('/register', (req, res) => {
-    res.render('register')
+    res.render('register');
 });
 // handle sign up login
 router.post('/register', (req, res) => {
     const newUser = new User({ username: req.body.username });
     User.register(newUser, req.body.password, (err, user) => {
         if (err) {
-            req.flash('error', err.message);
-            return res.render('register');
+            console.log(err)
+            return res.render('register',{error: err.message});
         }
         passport.authenticate('local')(req, res, () => {
-            req.flash('success', 'Welcome to YelpCamp ' + user.username);
+            req.flash('success', 'Successfully Signed Up! Nice to meet you ' + req.body.username);
             res.redirect('/campgrounds');
         });
     });
